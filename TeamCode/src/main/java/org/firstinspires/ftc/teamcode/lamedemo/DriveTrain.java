886e66795ed1b7ca38f8406cc2a7cbbc2f79af08
Sir Abe <102336201+AbeTheAmatuer@users.noreply.github.com>
2024-11-23 13:49:03 -0600

    Update DriveTrain.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lamedemo/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lamedemo/DriveTrain.java
@@ -22,8 +22,8 @@ public class DriveTrain {
         backRight = hardwareMap.get(DcMotor.class, "backRight");
 
         frontLeft.setDirection(DcMotor.Direction.FORWARD);
-        frontRight.setDirection(DcMotor.Direction.FORWARD);
-        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
         backRight.setDirection(DcMotor.Direction.REVERSE);
     }
 
